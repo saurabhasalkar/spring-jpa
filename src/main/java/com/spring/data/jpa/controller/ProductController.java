6bd1d83b6aa4fb6b414ca6a6cfa82914015def08
saurabhasalkar <saurabhasalkar98@gmail.com>
2024-10-20 08:10:21 +0530

    Spring data jpa operator, pagination and sorting examples

--- src/main/java/com/spring/data/jpa/controller/ProductController.java
+++ src/main/java/com/spring/data/jpa/controller/ProductController.java
@@ -3,6 +3,9 @@ package com.spring.data.jpa.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,63 +24,102 @@ import jakarta.annotation.PostConstruct;
 @RestController
 @RequestMapping("/products")
 public class ProductController {
-	
+
 	@Autowired
 	private ProductService service;
-	
-	
+
 	@PostMapping
 	public Product addProduct(@RequestBody Product product) {
 		return service.saveProduct(product);
 	}
 
-	
 	@GetMapping
-	public List<Product> getProducts(){
+	public List<Product> getProducts() {
 		return service.getProducts();
 	}
-	
+
 	@GetMapping("/id/{id}")
 	public Product getProductById(@PathVariable int id) {
 		return service.getProductById(id);
 	}
-	
-	
+
 	@GetMapping("/name/{name}")
 	public Product getProductByName(@PathVariable String name) {
 		return service.getProductByName(name);
-    }
-	
+	}
+
 	@GetMapping("/type/{productType}")
-	public List<Product> getProductByType(@PathVariable String productType){
+	public List<Product> getProductByType(@PathVariable String productType) {
 		return service.getProductByType(productType);
 	}
-	
-	
+
 	@GetMapping("/{price}/{productType}")
-	public List<Product> getProductWithPriceAndType(@PathVariable double price, @PathVariable String productType){
+	public List<Product> getProductWithPriceAndType(@PathVariable double price, @PathVariable String productType) {
 		return service.getProductWithPriceAndType(price, productType);
 	}
-	
-	
+
 	@GetMapping("/{price}")
-	public List<Product> getProductByPrice(@PathVariable double price){
-		
+	public List<Product> getProductByPrice(@PathVariable double price) {
+
 		return service.getProductByPrice(price);
-		
+
 	}
-	
+
 	@PutMapping("/{id}")
 	public Product updateProduct(@PathVariable int id, @RequestBody Product productRequest) {
 		return service.updateProduct(id, productRequest);
-		
+
 	}
-	
+
 	@DeleteMapping("/delete/{id}")
 	public long deleteProduct(@PathVariable int id) {
-		 
-		 return service.deleteProduct(id);
+
+		return service.deleteProduct(id);
 	}
-	
+
+	@PostMapping("/search")
+	public List<Product> getProductByMultiplePriceValue(@RequestBody List<Double> prices) {
+
+		return service.getProductByMultiplePriceValue(prices);
+	}
+
+	@GetMapping("/min/{minPrice}/max/{maxPrice}")
+	public List<Product> getProductByMultiplePriceRange(@PathVariable double minPrice, @PathVariable double maxPrice) {
+
+		return service.getProductByMultiplePriceRange(minPrice, maxPrice);
+	}
+
+	@GetMapping("/greater/{price}")
+	public List<Product> getProductWithHigherPrice(@PathVariable double price) {
+
+		return service.getProductWithHigherPrice(price);
+	}
+
+	@GetMapping("/less/{price}")
+	public List<Product> getProductWithLessPrice(@PathVariable double price) {
+
+		return service.getProductWithLessPrice(price);
+	}
+
+	@GetMapping("/like/{name}")
+	public List<Product> getProductWithLike(@PathVariable String name) {
+
+		return service.getProductWithLike(name);
+
+	}
+
+	@GetMapping("/sort/{fieldName}")
+	public List<Product> getProductWithSorting(@PathVariable String fieldName) {
+
+		return service.getProductWithSorting(fieldName);
+	}
+
+	// pagination
+
+	@GetMapping("/page/{offset}/{limit}")
+	public Page<Product> getPrdouctWithPageResponse(@PathVariable int offset, @PathVariable int limit) {
+
+		return service.getPrdouctWithPageResponse(offset, limit);
+	}
 
 }
--- src/main/java/com/spring/data/jpa/repository/ProductRepository.java
+++ src/main/java/com/spring/data/jpa/repository/ProductRepository.java
@@ -5,21 +5,28 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-
 import com.spring.data.jpa.entity.Product;
 
-public interface ProductRepository  extends JpaRepository<Product, Integer>{
-	
-	
+public interface ProductRepository extends JpaRepository<Product, Integer> {
+
 	Product findByName(String name);
 
 	List<Product> findByProductType(String productType);
 
-	
 	List<Product> findByPriceAndProductType(double price, String productType);
-	 
+
 //	@Query(value = "select * from product_tbl where price = ?1", nativeQuery = true)
-	
-	@Query("from Product p where p.price= ?1") //position parameter
+
+	@Query("from Product p where p.price= ?1") // position parameter
 	List<Product> getProductByPrice(double price);
+
+	List<Product> findByPriceIn(List<Double> prices);
+
+	List<Product> findByPriceBetween(double minPrice, double maxPrice);
+
+	List<Product> findAllByPriceGreaterThan(double price);
+
+	List<Product> findAllByPriceLessThan(double price);
+
+	List<Product> findByNameIgnoreCaseContaining(String name);
 }
